6523d6db924e11da32076f6a434de02ba15ba280
Fran Mella <92534421+mellaadev@users.noreply.github.com>
2022-03-23 23:55:55 +0100

    l

--- cliente/src/config/axios.js
+++ cliente/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const clienteAxios = axios.create({
-    baseURL : 'http://localhost:4000'
+    baseURL : 'https://peaceful-harbor-71878.herokuapp.com'
 });
 
 export default clienteAxios;
